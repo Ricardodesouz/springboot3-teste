8c43f2291107deffa409fdebf360f3605d706aaa
ricardo <ricardodesouza360@gmail.com>
2023-09-20 15:40:07 -0300

    Service layer, component registration

--- src/main/java/com/firtproject/project/repositories/UserRepository.java
+++ src/main/java/com/firtproject/project/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.firtproject.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.firtproject.project.entities.User;
-
+@Repository // opcional
 public interface UserRepository extends JpaRepository<User,Long>{
 
 }
